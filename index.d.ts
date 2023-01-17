591b9506274a11f73652a48026f9e0a492d8ab57
Remco <subs@klustre.nl>
2023-01-17 13:19:39 +0100

    Removes ctx from index

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,5 @@
 /// <reference lib="es2021" />
 /// <reference path="./types/api.d.ts" />
 /// <reference path="./types/cavalry.d.ts" />
-/// <reference path="./types/ctx.d.ts" />
 /// <reference path="./types/ui.d.ts" />
 /// <reference path="./types/browser.d.ts" />
